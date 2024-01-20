7341df523e90278d53e13dcae5b388ba981ac6fd
Seydus <weljohncatanpatan@gmail.com>
2024-01-20 13:10:43 +0800

    initial setup

---
+++ attendease/src/main/java/com/attendease/attendease/AttendeaseApplication.java
@@ -0,0 +1,13 @@
+package com.attendease.attendease;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttendeaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AttendeaseApplication.class, args);
+	}
+
+}
